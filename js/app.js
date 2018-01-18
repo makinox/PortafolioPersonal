41ce7d634c9fc0ad2922265efcd388a787774c8c
Jesus Bossa <jesusbossa@protonmail.com>
2018-01-18 17:38:21 -0500

    Nav-bar improved

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-"use strict"
-
-console.log('holis');
-
-let arreglo = [1,2,3,4,5,6,0]
-
-arreglo.forEach((numeros) => {
-  console.log(numeros);
+'use strict'
+$(document).ready(function(){
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(400)
+        $("nav").toggleClass('visible')
+        $(this).toggleClass('btn-open').toggleClass('btn-close')
+    })
+    $(".overlay a").click(function(){
+      $(".overlay").fadeToggle(400)
+      $("nav").toggleClass('visible')
+      $(".button a").toggleClass('btn-open').toggleClass('btn-close')
+    })
 })
