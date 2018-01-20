b15a4fc4bc3bd7101b577c85a4732f99b79c9d15
Jesus Bossa <jesusbossa@protonmail.com>
2018-01-19 20:23:46 -0500

    Se optimizo el tiempo de carga de la pagina

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict'
 
 let faded = () => {
-  $('.overlay').fadeToggle(200)
+  $('.overlay').fadeToggle(400)
   $('nav').toggleClass('visible')
   $('body').toggleClass('rueda')
+  $('#cuerpo').toggleClass('gaus')
   $('.button a').toggleClass('btn-open').toggleClass('btn-close')
 }
 
