c24785c9803163fe7636f1ba7eca3840a3814d21
Jesus bossa <jesusbossa@protonmail.com>
2018-09-20 21:43:07 -0500

    studies improved

--- src/studies(4)/index.js
+++ src/studies(4)/index.js
@@ -1,31 +1,92 @@
+import img1 from '../../assets/tech/Img1.svg'
+import img2 from '../../assets/tech/Img2.svg'
+import img3 from '../../assets/tech/Img3.svg'
+import img4 from '../../assets/tech/Img4.svg'
+import img5 from '../../assets/tech/Img5.svg'
+import img6 from '../../assets/tech/Img6.svg'
+import img7 from '../../assets/tech/Img7.svg'
+import img8 from '../../assets/tech/Img8.svg'
+import img9 from '../../assets/tech/Img9.svg'
+import img10 from '../../assets/tech/Img10.svg'
+import img11 from '../../assets/tech/Img11.svg'
+import img12 from '../../assets/tech/Img12.svg'
+import img13 from '../../assets/tech/Img13.svg'
+import img14 from '../../assets/tech/Img14.svg'
+import img15 from '../../assets/tech/Img15.svg'
+import img16 from '../../assets/tech/Img16.svg'
+import img17 from '../../assets/tech/Img17.svg'
+import img18 from '../../assets/tech/Img18.svg'
+import img19 from '../../assets/tech/Img19.svg'
+import img20 from '../../assets/tech/Img20.svg'
+import img21 from '../../assets/tech/Img21.svg'
+import img22 from '../../assets/tech/Img22.svg'
+
+
 export default () => (
     <section>
         <div>
             <h2>Que sé hasta ahora</h2>
         </div>
-        <div className="container">
-            <div className="item">
-                <p>Frontend</p>
+        <div className="card">
+            <div className="card-background">
+                <div className="card-container">
+                    <div><img src={img1} alt="" /></div>
+                    <div><img src={img2} alt="" /></div>
+                    <div><img src={img3} alt="" /></div>
+                    <div><img src={img4} alt="" /></div>
+                    <div><img src={img5} alt="" /></div>
+                    <div><img src={img6} alt="" /></div>
+                    <div><img src={img7} alt="" /></div>
+                    <div><img src={img8} alt="" /></div>
+                    <div><img src={img9} alt="" /></div>
+                    <div><img src={img10} alt="" /></div>
+                    <div><img src={img11} alt="" /></div>
+                    <div><img src={img12} alt="" /></div>
+                    <div><img src={img13} alt="" /></div>
+                    <div><img src={img14} alt="" /></div>
+                    <div><img src={img15} alt="" /></div>
+                    <div><img src={img16} alt="" /></div>
+                    <div><img src={img17} alt="" /></div>
+                    <div><img src={img18} alt="" /></div>
+                    <div><img src={img19} alt="" /></div>
+                    <div><img src={img20} alt="" /></div>
+                    <div><img src={img21} alt="" /></div>
+                    <div><img src={img22} alt="" /></div>
+                </div>
             </div>
-            <div className="item">
-                <p>Backend</p>
+            <div className="card-overlay">
+                <div>
+                    <h4>Puedes encontrar todos los soportes aquí</h4>
+                    <div className="btn">
+                        <a href="#">Certificados</a>
+                    </div>
+                </div>
             </div>
         </div>
-            <h4>Puedes encontrar todos los soportes aquí</h4>
-        <div className="btn">
-            <a href="#">Certificados</a>
-        </div>
-        <style jsx>
-            {`
-                .container {
+        <style jsx>{`
+
+                .card {
+                    position: relative;
+                    display: block;
+                    transition: 5s ease;
+                }
+
+                .card-background {
                     display: grid;
-                    grid-template-columns:  repeat(auto-fit , minmax(auto, 300px));
+                    grid-template-columns:  minmax(auto, 1080px);
                     justify-content: center;
                 }
 
-                .item {
-                    border: 1px red solid;
-                    height: 300px;
+                .card-container {
+                    display: grid;
+                    grid-template-columns:  repeat(auto-fit , minmax(auto, 110px));
+                    justify-content: center;
+                }
+
+                @media (max-width: 566px)  {
+                    .card-container {
+                        grid-template-columns: repeat(auto-fit , minmax(auto, 80px));
+                    }
                 }
 
                 a {
@@ -45,12 +106,40 @@ export default () => (
                 }
 
                 .btn {
-                    display: flex;
+                    display: grid;
                     justify-content: center;
+                }
+
+                .card-overlay {
+                    background: rgba(0,0,0, .6);
+                    position: absolute;
+                    color: white;
+                    top: 0;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    display: none;
+                    align-items: center;
+                    justify-content: center;
+                    text-align: center;
+                    transition: 5s ease;
+                }
+
+                .card:hover .card-overlay {
+                    display: flex;
+                    transition: 5s ease;
                 }
 
                 h4, h2 {
                     text-align: center;
+                }
+
+                img {
+                    cursor: pointer;
+                }
+
+                img:hover {
+                    transform: scale(1.05);
                 }
             `}
         </style>
