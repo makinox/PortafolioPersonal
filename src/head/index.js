e1af58d471b4e395cf1fda6968dafbb4e22938d1
Jesus Bossa <jesusbossa@protonmail.com>
2018-09-21 09:05:47 -0500

    Title added

--- src/head/index.js
+++ src/head/index.js
@@ -4,6 +4,7 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <title>Jesús Bossa | Ingeniero de software</title>
     </Head>
     <style jsx global>{`
         body {
