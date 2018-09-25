64e37d66e669240267f0cc9d15171f595be2c672
Jesus bossa <jesusbossa@protonmail.com>
2018-09-24 19:58:54 -0500

    Meta updated

--- src/head/index.js
+++ src/head/index.js
@@ -2,9 +2,19 @@ import Head from 'next/head'
 export default () => (
   <div>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="theme-color" content="#000000"></meta>
       <title>Jesús Bossa | Ingeniero de software</title>
+
+      <meta name="robots" content="index, follow" />
+      <meta name="revisit-after" content="3 days" />
+
+      <meta name="keywords" content="desarrollo, software, react, app, aplicaciones, moviles, android, iphone, ipad, windows, redux, construccion, arquitectura de software, ingeniero de software" />
+
+      <meta name="author" content="Jesús Bossa" />
+      <meta name="description" content="Jesús david bossa, ingeniero de software especializado en frontend" />
     </Head>
     <style jsx global>{`
         * {
