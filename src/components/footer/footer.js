6e8ef5e693d438a2e48f73c4e1dbbbdda187ecd7
Jesus bossa <x@jesusbossa.com>
2020-02-01 00:20:44 -0500

    [ADD] Google ads link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,6 +3,6 @@ import './footer.css'
 
 export default () => (
   <footer>
-      <h2>2018 - <a href="https://twitter.com/jesMakinox" target="_blank" rel="noopener noreferrer">Jesús Bossa</a></h2>
+      <h2>2020 - <a href="https://twitter.com/jesMakinox" target="_blank" rel="noopener noreferrer">Jesús Bossa</a></h2>
   </footer>
 )
