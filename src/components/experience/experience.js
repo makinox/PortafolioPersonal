88c6b247673d144e8303e63e8cc712d1d07cfe23
Jesus bossa <x@jesusbossa.com>
2020-02-06 15:43:03 -0500

    [ADD] New projects update

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -5,6 +5,9 @@ import img2 from '../../assets/exp/img2.jpg'
 import img3 from '../../assets/exp/img3.jpg'
 import img4 from '../../assets/exp/img4.jpg'
 import img5 from '../../assets/exp/img5.png'
+import img6 from '../../assets/exp/img6.jpeg'
+import img7 from '../../assets/exp/img7.jpeg'
+import img8 from '../../assets/exp/img8.jpeg'
 import message from '../lang/messages'
 
 export default (props) => (
@@ -105,6 +108,65 @@ export default (props) => (
                             <ul>
                                 <li>React-native</li>
                                 <li>Redux</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div >
+                <div className="experience-card">
+                    <div className="experience-card-header">
+                        <h4>Trips</h4>
+                        <p>{message[props.lang]['exp.sub1']}</p>
+                    </div>
+                    <div className="experience-card-body">
+                        <div className="experience-card-item experience-card-first">
+                        <a href="https://drive.google.com/open?id=16xQ6AhcL--BtdMiLrOAjFTo5G4kvPevU"><img src={img6} width={120} alt="Aplicacion de viajes" /></a>
+                        </div>
+                        <div className="experience-card-item experience-card-last">
+                            <ul>
+                                <li>Flutter</li>
+                                <li>BLoC</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div >
+                <div className="experience-card">
+                    <div className="experience-card-header">
+                        <h4>Darys</h4>
+                        <p>{message[props.lang]['exp.sub1']}</p>
+                    </div>
+                    <div className="experience-card-body">
+                        <div className="experience-card-item experience-card-first">
+                        <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.darys.jesusbossa"><img src={img7} width={120} alt="Aplicacion de viajes" /></a>
+                        </div>
+                        <div className="experience-card-item experience-card-last">
+                            <ul>
+                                <li>Ionic</li>
+                                <li>Capacitor</li>
+                                <li>Angular</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div >
+                <div className="experience-card">
+                    <div className="experience-card-header">
+                        <h4>Factiffy</h4>
+                        <p>{message[props.lang]['exp.sub1']}</p>
+                    </div>
+                    <div className="experience-card-body">
+                        <div className="experience-card-item experience-card-first">
+                        <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.jesusbossa.factiffy"><img src={img8} width={120} alt="Aplicacion de viajes" /></a>
+                        </div>
+                        <div className="experience-card-item experience-card-last">
+                            <ul>
+                                <li>Ionic</li>
+                                <li>Cordova</li>
+                                <li>Angular</li>
                             </ul>
                         </div>
                     </div>
