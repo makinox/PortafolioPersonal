337eb213a100411dc17714eea2e24a2419defdc9
Jesus bossa <x@jesusbossa.com>
2020-05-14 15:48:57 -0500

    [ADD] Experience component optimized

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -65,176 +65,103 @@ export default ({ lang }: { lang: 'en' | 'es' }) => {
     }
   `);
 
+  const proyects = [
+    {
+      subtitle: 'Regypos',
+      description: 'exp.sub1',
+      ref: 'https://www.regypos.com/',
+      imgName: 'img1',
+      imgAlt: 'Regypos creado por Jesus bossa',
+      techList: ['React', 'Bootstrap', 'PWA'],
+    },
+    {
+      subtitle: 'Inventario',
+      description: 'exp.sub1',
+      ref: 'https://inventory.regypos.com',
+      imgName: 'img2',
+      imgAlt: 'Inventario creado por Jesus bossa',
+      techList: ['React', 'Redux', 'Bootstrap', 'PWA'],
+    },
+    {
+      subtitle: 'Utopia',
+      description: 'exp.sub1',
+      ref: 'https://utopia1.herokuapp.com',
+      imgName: 'img3',
+      imgAlt: 'Aplicacion de utopia creada por Jesus bossa',
+      techList: ['VanillaJS', 'PWA'],
+    },
+    {
+      subtitle: 'Priorize',
+      description: 'exp.sub1',
+      ref: 'https://priorize.jesusbossa.dev/',
+      imgName: 'img4',
+      imgAlt: 'Aplicacion para tareas basicas por Jesus bossa',
+      techList: ['ReactJS', 'PWA'],
+    },
+    {
+      subtitle: 'MovieApp',
+      description: 'exp.sub1',
+      ref: 'https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40makinox/movieplayer-7392818119eeadcbdd885a0622b7063e-signed.apk',
+      imgName: 'img5',
+      imgAlt: 'Aplicacion para ver trailers de peliculas por Jesus bossa',
+      techList: ['React-native', 'Redux'],
+    },
+    {
+      subtitle: 'Factiffy',
+      description: 'exp.sub1',
+      ref: 'https://play.google.com/store/apps/details?id=com.jesusbossa.factiffy',
+      imgName: 'img8',
+      imgAlt: 'Aplicacion de red social por Jesus bossa',
+      techList: ['Ionic', 'Cordova', 'Angular'],
+    },
+    {
+      subtitle: 'Trips',
+      description: 'exp.sub1',
+      ref: 'https://drive.google.com/open?id=16xQ6AhcL--BtdMiLrOAjFTo5G4kvPevU',
+      imgName: 'img6',
+      imgAlt: 'Aplicacion de viajes por Jesus bossa',
+      techList: ['Flutter', 'BLoC'],
+    },
+    {
+      subtitle: 'Darys',
+      description: 'exp.sub1',
+      ref: 'https://play.google.com/store/apps/details?id=com.darys.jesusbossa',
+      imgName: 'img7',
+      imgAlt: 'Aplicacion de diario y notas por Jesus bossa',
+      techList: ['Ionic', 'Capacitor', 'Angular'],
+    },
+  ];
+
+  proyects.reverse();
+
   return (
     <section id="experience">
-      {/* {console.log(data)} */}
       <Title>
         <h2>{getMessage(lang, 'exp.title')}</h2>
       </Title>
       <Container>
-        <Card>
-          <div>
-            <Subtitle>Regypos</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub1')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a href="https://www.regypos.com/" target="_blanck">
-                <Image fixed={data.img1.childImageSharp.fixed} alt="Regypos creado por Jesus bossa" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>React</li>
-                <li>Bootstrap</li>
-                <li>PWA</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <div>
-            <Subtitle>Inventario</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub1')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a href="https://inventory.regypos.com/" target="_blanck">
-                <Image fixed={data.img2.childImageSharp.fixed} alt="Inventario creado por Jesus bossa" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>React</li>
-                <li>Redux</li>
-                <li>PWA</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <div>
-            <Subtitle>Utopia</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub1')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a href="https://utopia1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                <Image fixed={data.img3.childImageSharp.fixed} alt="Aplicacion de utopia creada por Jesus bossa" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>Vanilla</li>
-                <li>Bootstrap</li>
-                <li>PWA</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <div>
-            <Subtitle>Priorize</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub2')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a href="https://priorizex.firebaseapp.com/" target="_blank" rel="noopener noreferrer">
-                <Image fixed={data.img4.childImageSharp.fixed} alt="Aplicacion para tareas por hacer" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>React</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <div>
-            <Subtitle>MovieApp</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub1')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a href="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40makinox/movieplayer-7392818119eeadcbdd885a0622b7063e-signed.apk">
-                <Image fixed={data.img5.childImageSharp.fixed} alt="Aplicacion para tareas por hacer" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>React-native</li>
-                <li>Redux</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <div>
-            <Subtitle>Trips</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub1')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a href="https://drive.google.com/open?id=16xQ6AhcL--BtdMiLrOAjFTo5G4kvPevU">
-                <Image fixed={data.img6.childImageSharp.fixed} alt="Aplicacion de viajes" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>Flutter</li>
-                <li>BLoC</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <div>
-            <Subtitle>Darys</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub1')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.darys.jesusbossa">
-                <Image fixed={data.img7.childImageSharp.fixed} alt="Aplicacion de viajes" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>Ionic</li>
-                <li>Capacitor</li>
-                <li>Angular</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
-
-        <Card>
-          <div>
-            <Subtitle>Factiffy</Subtitle>
-            <Description>{getMessage(lang, 'exp.sub1')}</Description>
-          </div>
-          <CardBody>
-            <CardWrapper>
-              <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.jesusbossa.factiffy">
-                <Image fixed={data.img8.childImageSharp.fixed} alt="Aplicacion de viajes" />
-              </a>
-            </CardWrapper>
-            <CardWrapper>
-              <ul>
-                <li>Ionic</li>
-                <li>Cordova</li>
-                <li>Angular</li>
-              </ul>
-            </CardWrapper>
-          </CardBody>
-        </Card>
+        {proyects.map((el, index) => (
+          <Card key={index}>
+            <div>
+              <Subtitle>{el.subtitle}</Subtitle>
+              <Description>{getMessage(lang, 'exp.sub1')}</Description>
+            </div>
+            <CardBody>
+              <CardWrapper>
+                <a href={el.ref} target="_blanck">
+                  <Image fixed={data[el.imgName].childImageSharp.fixed} alt={el.imgAlt} />
+                </a>
+              </CardWrapper>
+              <CardWrapper>
+                <ul>
+                  {el.techList.map((te, idx) => (
+                    <li key={idx}>{te}</li>
+                  ))}
+                </ul>
+              </CardWrapper>
+            </CardBody>
+          </Card>
+        ))}
       </Container>
     </section>
   );
