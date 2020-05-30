159eb40af4ea79b48582cf5aa55b9147f4e24897
Jesús Bossa <x@jesusbossa.dev>
2020-05-30 17:30:05 -0500

    [IMPROVEMENT] Development port windows

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Portfolio`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@makinox`,
+    description: `Jesús Bossa | Portfolio`,
+    author: `Jesús Bossa`,
     siteUrl: 'https://jesusbossa.dev/',
   },
   plugins: [
