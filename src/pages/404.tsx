ba940d333f79c26a8e88bc357116cfdd7666e673
Jesus Bossa <x@jesusbossa.dev>
2021-04-03 12:29:07 -0500

    Improvement(project) 404 page improvements

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,21 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import { SEO } from '../components';
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }: { data: any }) => (
   <>
-    <SEO title="404: Not found" />
+    <SEO title="404: Not found" image={data.placeholderImage.publicURL} pathname="" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </>
 );
+
+export const query = graphql`
+  query {
+    placeholderImage: file(relativePath: { eq: "preview.png" }) {
+      publicURL
+    }
+  }
+`;
 
 export default NotFoundPage;
