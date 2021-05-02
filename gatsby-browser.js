9b76b1c5f048752075d0a5b78b12f383814328b7
Jesus Bossa <x@jesusbossa.dev>
2021-05-02 16:00:46 -0500

    chore: add makinox ui

---
+++ gatsby-browser.js
@@ -0,0 +1,1 @@
+require('./src/layout/colors.css');
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@makinox/makinox-ui';
 
 function IndexPage() {
   return (
     <main>
       <h1>Home Page</h1>
+      <Button use="contained" message="Say hi" onClick={() => alert('hi')} />
     </main>
   );
 }
