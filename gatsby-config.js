9349b9206aeed0f2e676787936fd0b61fd397b9f
Jesus Bossa <x@jesusbossa.dev>
2021-05-16 15:31:19 -0500

    feat: add dynamic fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,25 +8,32 @@ module.exports = {
     image: '/preview.png',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
-        icon: "src/images/icon.png",
+        fonts: [`Roboto\:300,400,500`, `Open Sans\:300,400,500`],
+        display: 'swap',
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "images",
-        path: "./src/images/",
+        icon: 'src/images/icon.png',
       },
-      __key: "images",
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
   ],
 };
