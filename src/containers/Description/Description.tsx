baa9d6dc689377f1475a14010fdca3ecc4203231
Jesus Bossa <x@jesusbossa.dev>
2021-05-23 11:53:38 -0500

    chore: description styles improved

--- src/containers/Description/Description.tsx
+++ src/containers/Description/Description.tsx
@@ -1,20 +1,34 @@
-import { Card, FluidContainer } from '@makinox/makinox-ui';
+import { DescriptionCard } from './Description.styles';
+import { FluidContainer } from '@makinox/makinox-ui';
 import React from 'react';
 
 function Description() {
   return (
     <FluidContainer className="flex justify-center">
-      <Card
+      <DescriptionCard
         use="elevated"
-        title="A little bit about me ..."
         maxWidth="600px"
-        text="I am a guy from Colombia who likes programming very much, usually curious about all that fantastic technology offers.
-
-During the last two years, I have dedicated myself to developing and innovating in technology projects, contributing to their development and future.
-
-I work in frontend, backend and devop. I build and design different types of software turning them into incredible products and services.
-
-Let me show you."
+        children={
+          <>
+            <div className="card-header">
+              <h6 className="headline6">A little bit about me ...</h6>
+            </div>
+            <div className="card-body">
+              <p className="body2">
+                I am a guy from Colombia who likes programming very much, usually curious about all that fantastic technology offers.
+              </p>
+              <p className="body2">
+                During the last five years, I have dedicated myself to developing and innovating in technology projects, contributing to their
+                development and future.
+              </p>
+              <p className="body2">
+                I work in frontend, backend and devops. I build and design different types of software turning them into incredible products and
+                services.
+              </p>
+              <p className="body2">Let me show you.</p>
+            </div>
+          </>
+        }
       />
     </FluidContainer>
   );
