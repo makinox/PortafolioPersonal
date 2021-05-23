8272b0e86c10397f14698b25f4c043edeea877c9
Jesus Bossa <x@jesusbossa.dev>
2021-05-23 15:21:43 -0500

    feat: add footer external links

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -12,10 +12,16 @@ function Footer() {
           <Button use="contained" message="x@jesusbossa.dev" onClick={() => window.open('mailto:x@jesusbossa.dev')} />
         </FooterContactContainer>
         <FooterSocialContainer>
-          <Float icon={<FaGithubSquare size={30} aria-label="Github" />} />
-          <Float icon={<FaInstagramSquare size={30} aria-label="Instagram" />} />
-          <Float icon={<FaLinkedin size={30} aria-label="Linkedin" />} />
-          <Float icon={<FaTwitterSquare size={30} aria-label="Twitter" />} />
+          <Float onClick={() => window.open('https://github.com/makinox', '_blank')} icon={<FaGithubSquare size={30} aria-label="Github" />} />
+          <Float
+            onClick={() => window.open('https://www.instagram.com/jesus.david7', '_blank')}
+            icon={<FaInstagramSquare size={30} aria-label="Instagram" />}
+          />
+          <Float
+            onClick={() => window.open('https://www.linkedin.com/in/makinox/', '_blank')}
+            icon={<FaLinkedin size={30} aria-label="Linkedin" />}
+          />
+          <Float onClick={() => window.open('https://twitter.com/jesMakinox', '_blank')} icon={<FaTwitterSquare size={30} aria-label="Twitter" />} />
         </FooterSocialContainer>
       </FluidContainer>
       <FooterContainer>
